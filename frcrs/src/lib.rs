09342585fbd9cf74e16a35ff6bf1cd52665142c4
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2022-09-08 20:29:40 -0500

    added makefile and start of rust side of project

---
+++ frcrs/src/lib.rs
@@ -0,0 +1,14 @@
+use std::convert::TryFrom;
+use std::result::Result;
+
+use j4rs::InvocationArg;
+use j4rs::prelude::*;
+use j4rs_derive::*;
+use serde::Deserialize;
+
+#[call_from_java("frc.robot.Main.rustentry")]
+fn entrypoint() { // called on rio boot
+    let jvm = Jvm::attach_thread().unwrap();
+
+    //jvm.invoke_static(class_name, method_name, inv_args)
+}
