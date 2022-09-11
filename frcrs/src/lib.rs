a6bbd43f2fed2bc1d6290b09cd0ec4f6eb0a10bf
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2022-09-10 20:15:40 -0500

    added drivers station warning to test response times are a little scary, a single method call is occasionally taking 10+ ms (avg ~2)

--- frcrs/src/lib.rs
+++ frcrs/src/lib.rs
@@ -6,10 +6,38 @@ use j4rs::prelude::*;
 use j4rs_derive::*;
 use serde::Deserialize;
 
+use std::time::SystemTime;
+
 #[call_from_java("frc.robot.Main.rustentry")]
 fn entrypoint() { // called on rio boot
     let jvm = Jvm::attach_thread().unwrap();
 
     //jvm.invoke_static(class_name, method_name, inv_args)
-    loop {}
+    jvm.invoke_static("edu.wpi.first.hal.HAL", "observeUserProgramStarting", &Vec::new()).unwrap();
+
+    let ds = jvm.invoke_static("edu.wpi.first.wpilibj.DriverStation", "getInstance", &Vec::new()).unwrap();
+
+
+    let mut now = SystemTime::now();
+    let mut delta = 0;
+
+    loop {
+        //jvm.invoke_static(
+        //    "edu.wpi.first.wpilibj.DriverStation", 
+        //    "reportWarning", 
+        //    &[InvocationArg::try_from("test warn").unwrap(),
+        //    InvocationArg::try_from(false).unwrap()]);
+        //jvm.invoke(&ds, "isTeleop",
+        //    &Vec::new());
+
+
+        jvm.invoke(&ds, "reportWarning",
+            &[InvocationArg::try_from(format!("test warn, {} ms", &delta)).unwrap(),
+            InvocationArg::try_from(false).unwrap().into_primitive().unwrap()]);
+
+        
+        delta = now.elapsed().unwrap().as_millis();
+        now = SystemTime::now();
+
+    }
 }
