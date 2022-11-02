368500e32739591d958063096f060c647a0fad1f
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2022-11-01 19:10:27 -0500

    cleanup

--- frcrs/frc-wrap/src/entry.rs
+++ frcrs/frc-wrap/src/entry.rs
@@ -1,37 +1,20 @@
 pub trait Entry {
-    fn init();
+    fn hal_start();
 }
 
 #[cfg(feature = "java")]
 pub mod entry {
     use j4rs::prelude::*;
     use lazy_static::lazy_static;
-    // This is a hack, it is intended to make the 
-    // transition to c++ bindings smoother
-    pub struct FrcJvm { 
-        jvm: j4rs::Jvm
-    }
+    pub struct FrcJvm { jvm: j4rs::Jvm }
     unsafe impl Sync for FrcJvm {}
     
-    //impl FrcJvm {
-    //    unsafe fn take(&mut self) -> j4rs::Jvm {
-    //        let jvm = replace(&mut self.jvm, None);
-    //        jvm.unwrap()
-    //    }
-    //}
-    //pub static mut FRC_JVM: FrcJvm = FrcJvm { jvm: None };
+    // This is a hack, it is intended to make the 
+    // transition to c++ bindings smoother
     lazy_static! {
         /// global JVM instance
-        static ref FRC_JVM: FrcJvm =  FrcJvm{jvm:Jvm::attach_thread().unwrap()};
+        static ref FRC_JVM: FrcJvm =  FrcJvm{ jvm: Jvm::attach_thread().unwrap() };
     }
-
-
-    /// Binds global JVM instance, call before any JNI calls
-    //pub fn init() -> Result<(), Box<dyn Error>>{
-    //    let mut jvm = FRC_JVM.0.lock()?;
-    //    *jvm = Some(FrcJvm(Jvm::attach_thread()?));
-    //    Ok(())
-    //}
 
     use super::Entry;
     pub use j4rs_derive::call_from_java as entrypoint;
@@ -39,9 +22,7 @@ pub mod entry {
     pub struct Java {  }
     impl Entry for Java {
         /// Call before anything else
-        fn init() {
-            //init().unwrap(); // setup JNI
-            
+        fn hal_start() {
             let jvm = &FRC_JVM.jvm;
 
             // ping driver's station
