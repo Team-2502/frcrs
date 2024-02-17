7b89c44fc0505fcbcbe69af704fd4a1a084fed52
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-02-17 10:24:54 -0600

    sendablechooser (untested)

--- src/networktables/smartdashboard.rs
+++ src/networktables/smartdashboard.rs
@@ -1,4 +1,6 @@
-use j4rs::{InvocationArg, Jvm};
+use std::collections::HashMap;
+
+use j4rs::{Instance, InvocationArg, Jvm};
 
 pub struct SmartDashboard;
 
@@ -41,5 +43,50 @@ impl SmartDashboard {
             ]
         )
         .unwrap();
+    }
+}
+
+pub struct Chooser<T> {
+    options: Vec<T>,
+    instance: Instance,
+}
+
+impl<T> Chooser<T> {
+    pub fn new() -> Self {
+        let options = Vec::new();
+        let jvm = Jvm::attach_thread().unwrap();
+
+        let instance = jvm.create_instance(
+            "edu.wpi.first.wpilibj.smartdashboard.SendableChooser",
+            &[ ]
+        ).unwrap();
+
+        Self { options, instance }
+    }
+
+    pub fn add(&mut self, name: &str, option: T) {
+        self.options.push(option);
+        let idx = self.options.len();
+        let jvm = Jvm::attach_thread().unwrap();
+
+        jvm.invoke(
+            &self.instance,
+            "addOption",
+            &[
+                InvocationArg::try_from(name.to_owned()).unwrap(),
+                InvocationArg::try_from(idx as i32).unwrap().into_primitive().unwrap(),
+            ],
+        ).unwrap();
+    }
+
+    pub fn get(&self) -> &T {
+        let jvm = Jvm::attach_thread().unwrap();
+        let idx: i32 = jvm.to_rust(jvm.invoke(
+            &self.instance,
+            "getSelected",
+            &Vec::new(),
+        ).unwrap()).unwrap();
+
+        &self.options[idx as usize]
     }
 }
