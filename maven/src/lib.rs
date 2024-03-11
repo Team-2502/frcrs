501c4c4a32b229cca774b764f35982b133c04259
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-03-11 14:50:47 -0500

    download test (works)

--- maven/src/lib.rs
+++ maven/src/lib.rs
@@ -1,1 +1,10 @@
+use package::Package;
+
 pub mod package;
+
+const WPI_MAVEN: Package<'static> = Package {
+    maven_url: "frcmaven.wpi.edu/artifactory/release",
+    version: "2024.2.1",
+    path: "edu.wpi.first",
+    name: ""
+};
--- maven/src/package.rs
+++ maven/src/package.rs
@@ -1,22 +1,33 @@
-pub struct Package {
-    pub maven_url: String,
-    pub group: String,
-    pub name: String,
-    pub version: String,
+use std::{env, path::{Path, PathBuf}, io::Cursor, str::FromStr};
+
+#[derive(Default)]
+pub struct Package<'a> {
+    pub maven_url: &'a str,
+    pub path: &'a str,
+    pub name: &'a str,
+    pub version: &'a str,
 }
 
 pub struct Artifact<'a> {
-    pub package: &'a Package,
-    pub name: String,
+    pub package: &'a Package<'a>,
+    pub name: &'a str,
 }
 
-impl Package {
+impl<'a> Package<'a> {
     fn url(&self) -> String {
-        format!("https://{}/{}/{}/{}", self.maven_url, self.group.replace(".", "/"), self.name, self.version)
+        format!("https://{}/{}/{}/{}", self.maven_url, self.path.replace(".", "/"), self.name, self.version)
     }
 
-    fn artifact(&self, name: String) -> Artifact {
+    fn artifact(&'a self, name: &'a str) -> Artifact<'a> {
         Artifact { package: &self, name }
+    }
+
+    fn download_headers(&self) -> anyhow::Result<()> {
+
+        let path = PathBuf::from_str(std::env::var("OUT_DIR")?.as_str())?;
+        self.artifact("headers").download(&path)?;
+
+        Ok(())
     }
 }
 
@@ -25,7 +36,33 @@ impl<'a> Artifact<'a> {
         format!("{}/{}-{}-{}.zip", self.package.url(), self.package.name, self.package.version, self.name)
     }
 
-    pub fn download(&self) {
-        dbg!(self.url());
+    pub fn download(&self, path: &Path) -> anyhow::Result<()> {
+        let data = reqwest::blocking::get(self.url())?.bytes()?;
+        let out = std::env::var("OUT_DIR")?;
+        let path = Path::new(&out);
+        zip_extract::extract(Cursor::new(data), &path, true)?;
+
+        Ok(())
     }
+}
+
+
+mod tests {
+    use crate::WPI_MAVEN;
+
+    use super::Package;
+
+
+    #[test]
+    fn download() {
+        let hal = Package {
+            path: "edu.wpi.first.hal",
+            name: "hal-cpp",
+            ..WPI_MAVEN
+        };
+
+        hal.download_headers().unwrap();
+        println!("{}", std::env::var("OUT_DIR").unwrap());
+    }
+
 }
