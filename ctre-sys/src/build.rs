ce04e2b93b29a72900b2c8c439cfe69151371e5d
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-03-11 20:33:34 -0500

    bindgen cross compilation (pain)

--- ctre-sys/src/build.rs
+++ ctre-sys/src/build.rs
@@ -44,6 +44,7 @@ fn main() -> anyhow::Result<()> {
         ..CTRE_MAVEN
     };
 
+
     for package in dependencies.iter().chain([&ctre_wpi,&ctre_tools].into_iter()) {
         package.download_headers()?;
     }
@@ -57,6 +58,10 @@ fn main() -> anyhow::Result<()> {
         .clang_arg("-xc++")
         .clang_arg("-std=c++20")
         .clang_arg(format!("-I{}/include", out))
+        .clang_arg(format!("--sysroot={}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot", env!("HOME")))
+        .clang_arg(format!("-I{}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot/usr/include", env!("HOME")))
+        .clang_arg(format!("-I{}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot/usr/include/c++/12", env!("HOME")))
+        .clang_arg(format!("-I{}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot/usr/include/c++/12/arm-nilrt-linux-gnueabi", env!("HOME")))
         .header("src/wrapper.hpp")
         .allowlist_file("wrapper.hpp")
         .allowlist_item("test")
@@ -68,7 +73,23 @@ fn main() -> anyhow::Result<()> {
         .generate()?
         .write_to_file(format!("{}/bindings.rs", out))?;
 
-    println!("cargo:rustc-link-search=native={}{}", out, "libs");
+/*
+    cc::Build::new()
+        .cpp(true)
+        .std("c++20")
+        .compiler("arm-frc2024-linux-gnueabi-c++")
+        .cpp_link_stdlib("stdc++")
+        .flag(&format!("--isysroot={}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot", env!("HOME")))
+        .flag(&format!("--with-native-headers={}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot/usr/include/c++/12", env!("HOME")))
+        .include(format!("{}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot/usr/include", env!("HOME")))
+        .include(format!("{}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot/usr/include/c++/12", env!("HOME")))
+        .include(format!("{}/include", out))
+        .file("src/wrapper.hpp")
+        .compile("ctre-wrapper");
+
+        */
+    //println!("cargo:rustc-link-search=native={}/.gradle/toolchains/frc/2024/roborio/arm-nilrt-linux-gnueabi/sysroot/usr/include", env!("HOME"));
+    println!("cargo:rustc-link-search=native={}/{}", out, "libs");
 
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=src/wrapper.hpp");
