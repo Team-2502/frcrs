bfb0e052ea4bd03d562a09ce994d936be680e3f1
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-03-13 11:24:09 -0500

    dio

--- src/dio.rs
+++ src/dio.rs
@@ -1,32 +1,25 @@
+use hal_sys::{HAL_GetDIO, HAL_InitializeDIOPort, HAL_PortHandle, HAL_Handle, HAL_FreeDIOPort};
 use j4rs::{Jvm, InvocationArg, Instance};
 
 pub struct DIO {
-    instance: Instance,
+    handle: i32,
 }
 
 impl DIO {
     pub fn new(port: i32) -> Self { 
-        let jvm = Jvm::attach_thread().unwrap();
-
-        let instance = jvm
-            .create_instance(
-                "edu.wpi.first.wpilibj.DigitalInput",
-                &[
-                    InvocationArg::try_from(port)
-                        .unwrap()
-                        .into_primitive()
-                        .unwrap(),
-                ],
-            )
-            .unwrap();
-        Self { instance } 
+        let mut _ret = 0;
+        let handle = unsafe {HAL_InitializeDIOPort(port, 1, std::ptr::null(), &mut _ret)};
+        Self { handle } 
     }
 
     pub fn get(&self) -> bool {
-        let jvm = Jvm::attach_thread().unwrap();
+        let mut _ret = 0;
+        unsafe{HAL_GetDIO(self.handle, &mut _ret) == 1}
+    }
+}
 
-        jvm
-            .to_rust(jvm.invoke(&self.instance, "get", &Vec::new()).unwrap())
-            .unwrap()
+impl Drop for DIO {
+    fn drop(&mut self) {
+        unsafe{HAL_FreeDIOPort(self.handle)}
     }
 }
