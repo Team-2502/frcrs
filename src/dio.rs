e4b91dcd74b3b54d6e7e4d0ed43a1c39260c5ffd
Andy Killorin <37423245+Speedy6451@users.noreply.github.com>
2024-03-22 21:02:32 -0500

    digitaloutput

--- src/dio.rs
+++ src/dio.rs
@@ -30,3 +30,44 @@ impl DIO {
             .unwrap()
     }
 }
+
+pub struct DO {
+    instance: Instance,
+}
+
+impl DO {
+    pub fn new(port: i32) -> Self { 
+        let jvm = Jvm::attach_thread().unwrap();
+
+        let instance = jvm
+            .create_instance(
+                "edu.wpi.first.wpilibj.DigitalOutput",
+                &[
+                    InvocationArg::try_from(port)
+                        .unwrap()
+                        .into_primitive()
+                        .unwrap(),
+                ],
+            )
+            .unwrap();
+        Self { instance } 
+    }
+
+    pub fn get(&self) -> bool {
+        let jvm = Jvm::attach_thread().unwrap();
+
+        jvm
+            .to_rust(jvm.invoke(&self.instance, "get", &Vec::new()).unwrap())
+            .unwrap()
+    }
+
+    pub fn set(&self, value: bool) {
+        let jvm = Jvm::attach_thread().unwrap();
+        println!("crash");
+
+        jvm.invoke(&self.instance, "set", &[
+            InvocationArg::try_from(value)
+            .unwrap().into_primitive().unwrap()
+        ]).unwrap();
+    }
+}
