6744a419fdeb5c05c57ffc3558d7248c71a97c82
Nolan <77746688+Sha-dos@users.noreply.github.com>
2024-11-27 15:45:22 -0600

    use new constructors from auto-jni

--- src/ctre/mod.rs
+++ src/ctre/mod.rs
@@ -8,28 +8,12 @@ pub struct TalonFX {
     motor: com_ctre_phoenix6_hardware_TalonFX,
 }
 
-fn create_talon(id: i32, can_bus: Option<String>) -> GlobalRef {
-    let string = java().new_string(can_bus.unwrap_or("rio".to_string())).unwrap();
-
-    let instance = create!(
-            "com/ctre/phoenix6/hardware/TalonFX",
-            "(ILjava/lang/String;)V",
-            &[JValue::Int(id).as_jni(),
-                JValue::Object(&JObject::from_raw(string.into_raw())).as_jni()
-            ]
-        );
-
-    java().new_global_ref(instance).unwrap()
-}
-
 impl TalonFX {
     pub fn new(id: i32, bus: Option<String>) -> Self {
-        let mut motor = com_ctre_phoenix6_hardware_TalonFX {
-            inner: create_talon(id, bus),
-        };
+        let string = java().new_string(bus.unwrap_or("rio".to_string())).unwrap();
 
         Self {
-            motor
+            motor: com_ctre_phoenix6_hardware_TalonFX::new_2(id, JObject::from(string)).unwrap()
         }
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[allow(non_camel_case_types)]
+#[allow(unused_variables)]
+#[allow(unused)]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 mod hal;
